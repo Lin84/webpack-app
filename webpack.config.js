5401497990f9637d03dab19c4846ef17e8c94bd4
Linh Le Tuan <linh.tuan@actum.cz>
2017-08-13 20:32:26 +0200

    multiple html

--- webpack.config.js
+++ webpack.config.js
@@ -52,6 +52,18 @@ module.exports = {
                         }
                     }
                 ]
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                        }
+                    }
+                ],
+                exclude: path.resolve(__dirname, 'src/index.html')
             }
         ]
     },
